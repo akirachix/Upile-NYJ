dc6591505ab7124bcecfb750c1810c75a95b6e24
Mutava-Ndinda <ndindafmutava@gmail.com>
2024-07-14 11:33:45 +0300

    added videos

---
+++ multimedia-project/src/GoogleVideo/index.js
@@ -0,0 +1,44 @@
+import './index.css';
+
+
+const Video=()=>{
+    return(
+        <>
+        <h1><span>Watch</span><span id='red'>Here</span></h1>
+        <div id='videos'>
+            <div id='map'>
+    <a href="https://www.loom.com/share/d3e0a92247ef48c88c2d45bdce0a99c8">
+     
+    </a>
+    <a href="https://www.loom.com/share/d3e0a92247ef48c88c2d45bdce0a99c8">
+      <img id='googlemap' src="https://cdn.loom.com/sessions/thumbnails/d3e0a92247ef48c88c2d45bdce0a99c8-with-play.gif" alt="Google Maps"/>
+    </a>
+  </div>
+
+
+  <div id='features'>
+    <a href="https://www.loom.com/share/9d5502cc6f67430ea7a8715ea657a978">
+      
+    </a>
+    <a href="https://www.loom.com/share/9d5502cc6f67430ea7a8715ea657a978">
+      <img id='googlefeatures' src="https://cdn.loom.com/sessions/thumbnails/9d5502cc6f67430ea7a8715ea657a978-with-play.gif" alt="Google Features"/>
+    </a>
+  </div>
+
+
+            <div id='architecture'>
+    <a href="https://www.loom.com/share/1560808b5a804ed59f486252f092a8a1">
+      
+    </a>
+    <a href="https://www.loom.com/share/1560808b5a804ed59f486252f092a8a1">
+      <img id='googlearch' src="https://cdn.loom.com/sessions/thumbnails/1560808b5a804ed59f486252f092a8a1-with-play.gif" alt="Google Architecture"/>
+    </a>
+  </div>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default Video;
