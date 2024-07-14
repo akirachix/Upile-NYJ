9892687dae9a38f9292202adb59b25e1bd3d46bb
Hannah-Unyolo <unyolohannah@gmail.com>
2024-07-14 16:08:01 +0300

    made changes

--- multimedia-project/src/App.js
+++ multimedia-project/src/App.js
@@ -1,18 +1,14 @@
-import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+
 import Blog from "./Blog";
 import React from 'react';
 
-import Summary from "./Article";
 
 function App(){
   return(
 
-<Router>
+<div>
   <Blog/>
-  <Routes>
-    <Route path="/read-more" element={<Summary/>}></Route>
-  </Routes>
-</Router>
+</div>
   
 
   )
